4464ede2b6d7aa6f2b9c84a00c1fd38bae537084
joehall87 <joehall87@gmail.com>
2021-11-27 13:59:47 +0000

    Fix theme swatch

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,23 @@ import 'package:provider/provider.dart';
 import 'package:fairteams/state.dart';
 import 'package:fairteams/splash.dart';
 
+MaterialColor swatch = const MaterialColor(
+  // Generated from https://maketintsandshades.com/#1976D2
+  0xff1976d2, // Colors.blue[700]
+  <int, Color>{
+    50: Color(0xff75ade4),
+    100: Color(0xff75ade4),
+    200: Color(0xff5e9fe0),
+    300: Color(0xff4791db),
+    400: Color(0xff3084d7),
+    500: Color(0xff1976d2), // Colors.blue[700]
+    600: Color(0xff176abd),
+    700: Color(0xff145ea8),
+    800: Color(0xff125393),
+    900: Color(0xff0f477e),
+  },
+);
+
 void main() {
   runApp(ChangeNotifierProvider(
       create: (context) => AppState(), child: const App()));
@@ -15,7 +32,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = ThemeData(
-      primaryColor: Colors.blue[700],
+      primarySwatch: swatch,
       fontFamily: 'Montserrat',
       floatingActionButtonTheme:
           // this defaults to the secondary color otherwise
